666d29b61c89d408c6d720a7ec036e7f58ce6c60
2mat0 <mat@overclockers.at>
2011-02-02 18:37:20 +0100

    *) Initial commit

---
+++ TweakTool/MainForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace TweakTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageDrivers = new System.Windows.Forms.TabPage();
+            this.listDrivers = new System.Windows.Forms.ListView();
+            this.columnDeviceName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDriverDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDriverVersion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnManufacturer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnInfName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelDrivers = new System.Windows.Forms.Label();
+            this.buttonRemoveSelectedDrivers = new System.Windows.Forms.Button();
+            this.tabPageInfo = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabelOC = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPageDrivers.SuspendLayout();
+            this.tabPageInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageDrivers);
+            this.tabControl1.Controls.Add(this.tabPageInfo);
+            this.tabControl1.Location = new System.Drawing.Point(-3, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(644, 377);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPageDrivers
+            // 
+            this.tabPageDrivers.Controls.Add(this.listDrivers);
+            this.tabPageDrivers.Controls.Add(this.labelDrivers);
+            this.tabPageDrivers.Controls.Add(this.buttonRemoveSelectedDrivers);
+            this.tabPageDrivers.Location = new System.Drawing.Point(4, 22);
+            this.tabPageDrivers.Name = "tabPageDrivers";
+            this.tabPageDrivers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageDrivers.Size = new System.Drawing.Size(636, 351);
+            this.tabPageDrivers.TabIndex = 0;
+            this.tabPageDrivers.Text = "Drivers";
+            this.tabPageDrivers.UseVisualStyleBackColor = true;
+            // 
+            // listDrivers
+            // 
+            this.listDrivers.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.listDrivers.AllowColumnReorder = true;
+            this.listDrivers.CheckBoxes = true;
+            this.listDrivers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnDeviceName,
+            this.columnDriverDate,
+            this.columnDriverVersion,
+            this.columnManufacturer,
+            this.columnInfName});
+            this.listDrivers.FullRowSelect = true;
+            this.listDrivers.Location = new System.Drawing.Point(11, 23);
+            this.listDrivers.Name = "listDrivers";
+            this.listDrivers.Size = new System.Drawing.Size(614, 291);
+            this.listDrivers.TabIndex = 2;
+            this.listDrivers.UseCompatibleStateImageBehavior = false;
+            this.listDrivers.View = System.Windows.Forms.View.Details;
+            // 
+            // columnDeviceName
+            // 
+            this.columnDeviceName.Text = "Device";
+            this.columnDeviceName.Width = 250;
+            // 
+            // columnDriverDate
+            // 
+            this.columnDriverDate.Text = "Driver Date";
+            this.columnDriverDate.Width = 70;
+            // 
+            // columnDriverVersion
+            // 
+            this.columnDriverVersion.Text = "Version";
+            this.columnDriverVersion.Width = 100;
+            // 
+            // columnManufacturer
+            // 
+            this.columnManufacturer.Text = "Manufacturer";
+            this.columnManufacturer.Width = 120;
+            // 
+            // columnInfName
+            // 
+            this.columnInfName.Text = "Inf File";
+            // 
+            // labelDrivers
+            // 
+            this.labelDrivers.AutoSize = true;
+            this.labelDrivers.Location = new System.Drawing.Point(5, 7);
+            this.labelDrivers.Name = "labelDrivers";
+            this.labelDrivers.Size = new System.Drawing.Size(43, 13);
+            this.labelDrivers.TabIndex = 4;
+            this.labelDrivers.Text = "Drivers:";
+            // 
+            // buttonRemoveSelectedDrivers
+            // 
+            this.buttonRemoveSelectedDrivers.Location = new System.Drawing.Point(242, 320);
+            this.buttonRemoveSelectedDrivers.Name = "buttonRemoveSelectedDrivers";
+            this.buttonRemoveSelectedDrivers.Size = new System.Drawing.Size(153, 23);
+            this.buttonRemoveSelectedDrivers.TabIndex = 3;
+            this.buttonRemoveSelectedDrivers.Text = "Remove selected drivers";
+            this.buttonRemoveSelectedDrivers.UseVisualStyleBackColor = true;
+            this.buttonRemoveSelectedDrivers.Click += new System.EventHandler(this.buttonRemoveSelectedDrivers_Click);
+            // 
+            // tabPageInfo
+            // 
+            this.tabPageInfo.Controls.Add(this.label2);
+            this.tabPageInfo.Controls.Add(this.linkLabelOC);
+            this.tabPageInfo.Controls.Add(this.label1);
+            this.tabPageInfo.Controls.Add(this.pictureBox1);
+            this.tabPageInfo.Location = new System.Drawing.Point(4, 22);
+            this.tabPageInfo.Name = "tabPageInfo";
+            this.tabPageInfo.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageInfo.Size = new System.Drawing.Size(636, 351);
+            this.tabPageInfo.TabIndex = 1;
+            this.tabPageInfo.Text = "Info";
+            this.tabPageInfo.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(23, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(566, 58);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "This tool can destroy your system configuration.\r\nMake backups and use it at your" +
+                " own risk!";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkLabelOC
+            // 
+            this.linkLabelOC.AutoSize = true;
+            this.linkLabelOC.Location = new System.Drawing.Point(249, 309);
+            this.linkLabelOC.Name = "linkLabelOC";
+            this.linkLabelOC.Size = new System.Drawing.Size(138, 13);
+            this.linkLabelOC.TabIndex = 2;
+            this.linkLabelOC.TabStop = true;
+            this.linkLabelOC.Text = "http://www.overclockers.at";
+            this.linkLabelOC.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelOC_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A project by the community of:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TweakTool.Properties.Resources.oc_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(592, 144);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 372);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainForm";
+            this.Text = "overclockers.at TweakTool";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageDrivers.ResumeLayout(false);
+            this.tabPageDrivers.PerformLayout();
+            this.tabPageInfo.ResumeLayout(false);
+            this.tabPageInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageDrivers;
+        private System.Windows.Forms.TabPage tabPageInfo;
+        private System.Windows.Forms.Label labelDrivers;
+        private System.Windows.Forms.Button buttonRemoveSelectedDrivers;
+        private System.Windows.Forms.ListView listDrivers;
+        private System.Windows.Forms.ColumnHeader columnDeviceName;
+        private System.Windows.Forms.ColumnHeader columnDriverDate;
+        private System.Windows.Forms.ColumnHeader columnDriverVersion;
+        private System.Windows.Forms.ColumnHeader columnManufacturer;
+        private System.Windows.Forms.ColumnHeader columnInfName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabelOC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ TweakTool/MainForm.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace TweakTool
+{
+    public partial class MainForm : Form
+    {
+        // DIFx framework binding
+        [DllImport("DIFxAPI.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern Int32 DriverPackageUninstall([MarshalAs(UnmanagedType.LPTStr)] string DriverPackageInfPath, Int32 Flags, IntPtr pInstallerInfo, out bool pNeedReboot);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct INSTALLERINFO
+        {
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string pApplicationId;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string pDisplayName;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string pProductName;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string pMfgName;
+        }
+
+        const Int32 DRIVER_PACKAGE_REPAIR = 0x00000001;
+        const Int32 DRIVER_PACKAGE_SILENT = 0x00000002;
+        const Int32 DRIVER_PACKAGE_FORCE = 0x00000004;
+        const Int32 DRIVER_PACKAGE_ONLY_IF_DEVICE_PRESENT = 0x00000008;
+        const Int32 DRIVER_PACKAGE_LEGACY_MODE = 0x00000010;
+        const Int32 DRIVER_PACKAGE_DELETE_FILES = 0x00000020;
+
+        const Int32 ERROR_SUCCESS = 0;
+        const Int32 ERROR_FILE_NOT_FOUND = 2;
+        const Int32 ERROR_ACCESS_DENIED = 5;
+        const Int32 ERROR_CANT_ACCESS_FILE = 1920;
+        // A lot of error codes are missing here ... to map more of them see: http://msdn.microsoft.com/en-us/library/ms681381(v=VS.85).aspx?appId=Dev10IDEF1&l=EN-US&k=k
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private bool GetDrivers()
+        {
+            // *) Remove old entries from list
+            listDrivers.Items.Clear();
+
+            // *) Hourglass cursor
+            Cursor.Current = Cursors.WaitCursor;
+
+            // *) Query WMI interface for signed drivers (For all properties see: http://msdn.microsoft.com/en-us/library/aa394354(v=VS.85).aspx)
+            System.Management.SelectQuery oQuery                 = new System.Management.SelectQuery("Win32_PnPSignedDriver");
+            System.Management.ManagementObjectSearcher oSearcher = new System.Management.ManagementObjectSearcher(oQuery);
+
+            // *) Loop through query results
+            foreach (System.Management.ManagementObject oManageObject in oSearcher.Get())
+            {
+                // -) Check for valid result
+                String szInfName = oManageObject["InfName"] != null ? oManageObject["InfName"].ToString() : "";
+                if (!szInfName.StartsWith("oem") || !szInfName.EndsWith(".inf"))
+                // => No third party driver
+                    continue;
+
+                // -) Convert driver date
+                String szDriverDate = oManageObject["DriverDate"] != null ? 
+                    System.Management.ManagementDateTimeConverter.ToDateTime(oManageObject["DriverDate"].ToString()).ToString("dd.MM.yyyy") :
+                    "n/a";
+                
+                // -) Add items to our list
+                //
+                // => Create main item
+                ListViewItem oListItem1 = new ListViewItem(oManageObject["DeviceName"] != null ? oManageObject["DeviceName"].ToString() : "n/a");
+
+                // => Subitem collection for additional columns
+                ListViewItem.ListViewSubItemCollection oListCollection = new ListViewItem.ListViewSubItemCollection(oListItem1);
+
+                ListViewItem.ListViewSubItem oListItem2 = new ListViewItem.ListViewSubItem(oListItem1, szDriverDate);
+                ListViewItem.ListViewSubItem oListItem3 = new ListViewItem.ListViewSubItem(oListItem1, oManageObject["DriverVersion"] != null ? oManageObject["DriverVersion"].ToString() : "n/a");
+                ListViewItem.ListViewSubItem oListItem4 = new ListViewItem.ListViewSubItem(oListItem1, oManageObject["Manufacturer"] != null ? oManageObject["Manufacturer"].ToString() : "n/a");
+                ListViewItem.ListViewSubItem oListItem5 = new ListViewItem.ListViewSubItem(oListItem1, szInfName);
+
+                // => Add subitem collection
+                oListCollection.Add(oListItem2);
+                oListCollection.Add(oListItem3);
+                oListCollection.Add(oListItem4);
+                oListCollection.Add(oListItem5);
+
+                // => Add new row to driver list
+                listDrivers.Items.Add(oListItem1);
+            }
+
+            // *) Return to normal cursor
+            Cursor.Current = Cursors.Default;
+
+            return true;
+        }
+
+        // Calls DriverPackageUninstall() from DIFx framework (See: http://msdn.microsoft.com/en-us/library/ff544822(v=VS.85).aspx)
+        private Int32 UninstallDriver(string szInfFile, ref bool bNeedReboot)
+        {
+            Int32 nFlags = DRIVER_PACKAGE_SILENT | DRIVER_PACKAGE_DELETE_FILES;
+
+            return DriverPackageUninstall(szInfFile, nFlags, IntPtr.Zero, out bNeedReboot);
+        }
+
+        private void buttonRemoveSelectedDrivers_Click(object sender, EventArgs e)
+        {
+            if (listDrivers.CheckedItems.Count == 0)
+                return;
+
+            // *) Hourglass cursor
+            Cursor.Current = Cursors.WaitCursor;
+
+            // *) Get Windows inf path
+            string szInfPath = Environment.GetEnvironmentVariable("SystemRoot") + @"\inf";
+
+            // *) Loop trough list items
+            string[] szErrorInfs  = new string[listDrivers.Items.Count];
+            bool bRebootNeeded    = false;
+
+            int nErrors      = 0,
+                nUninstalled = 0;
+
+            foreach (ListViewItem oListItem in listDrivers.CheckedItems)
+            {
+                if (!oListItem.Checked)
+                // => not checked
+                    continue;
+
+                // -) Fetch inf filename from subitems
+                ListViewItem.ListViewSubItem oListItem5 = oListItem.SubItems[4];
+
+                string szInfFile = oListItem5.Text;
+
+                // -) Uninstall driver
+                bool bNeedReboot = false;
+
+                Int32 nResult = UninstallDriver(szInfPath+@"\"+szInfFile, ref bNeedReboot);
+                if (nResult != ERROR_SUCCESS)
+                // => This part of the code could use definitely a better error processing!
+                    szErrorInfs[nErrors++] = szInfFile + " (" + nResult.ToString() + ")";
+                else
+                {
+                    nUninstalled++;
+
+                    if (bNeedReboot)
+                        bRebootNeeded = true;
+                }
+            }
+
+            // *) Return to normal cursor
+            Cursor.Current = Cursors.Default;
+
+            // *) Show error messages
+            if (nErrors > 0)
+            {
+                // -) Implode erroneous inf files to one string
+                string szErrors = "";
+
+                foreach (string szError in szErrorInfs)
+                {
+                    if (szErrors != "")
+                        szErrors += " ";
+
+                    szErrors += szError;
+                }
+
+                // -) Show awful messagebox
+                MessageBox.Show("Could not uninstall following drivers: "+szErrors,"Error",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            }
+            else
+            if (nUninstalled > 0)
+                MessageBox.Show(nUninstalled.ToString()+" drivers successfully removed from the driver store!","Notice",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            // *) Reboot notice
+            if (bRebootNeeded)
+            {
+                if (MessageBox.Show("You should reboot now to see any changes! Save your data and continue ...", "Notice", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process.Start("ShutDown", "/r");
+                    return;
+                }
+            }
+
+            // *) Reload drivers
+            if (!GetDrivers())
+                MessageBox.Show("Could not reload drivers!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            if (!GetDrivers())
+                MessageBox.Show("Could not load drivers!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void linkLabelOC_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.overclockers.at");
+        }
+    }
+}
